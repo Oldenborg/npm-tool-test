bfbcaf2bdedb134d2bd582ebb497ea6f660a8bb3
Henrik Oldenborg <henrik@oldenborg.nu>
2017-05-22 10:08:14 +0200

    Working example

--- bin/make.js
+++ bin/make.js
@@ -46,99 +46,91 @@ require('yargs')
 
       console.log('File name:', name);
       console.log('Pages:', pages);
-      createFile('./bin/','router.js');
 
       for(var i = 0, len = pages.length; i < len; i++) {
         var page = pages[i];
 
-            createPugTemplate(name,page);
-            createJsViewPageExtension(name,page);
+            if (page == 'index') {
+                createPugIndex(name,page);
+                // createJsIndex();
+                // createRoutesIndex();
+            } else if (page == 'edit') {
+                // createPugEdit();
+                // createJsEdit();
+                // createRoutesEdit();
+            } else if (page == 'new') {
+                // createPugNew();
+                // createJsNew();
+                // createRoutesNew();
+            } else {
+                throw new Error('Unknown page type, you must define what should happen when creating a page with the type: ' + page);
+            }
       }
     })
     .help()
     .argv
 
-function getTemplateId(name,page) {
-
-    var name = name.toUnderscore().replace(/\./g,'_');
-    return name + '_' + page;
-}
-
-function getPath(name) {
- return name.toUnderscore().replace(/\./g,'/');
-}
-
-function createPugTemplate(name,page) {
+function createPugIndex(name,page) {
 
     var path = getPath(name);
     var file = getFileName(page,'pug');
+    var basepath = './app/pug/' + path;
     var template_id = getTemplateId(name,page);
-    var fullpath = './app/pug/' + path + '/' + file;
 
 var template = `script(type="text/template", id="${template_id}")
   .row
     .col-xs-12
       .box.box--white
         .box__controls
-          a.btn.btn-sm(href="/#production/interfaces/new/") Create
+          a.btn.btn-sm(href="/#${path}/new/", data-l="button.new")
  
         .box__body
-          h3 Production - Interfaces index page
-          a(href="/#production/interfaces/new/") Link to create page
+          h3 ${name} - ${page} page
+          a(href="/#${path}/new/") Link to create page
           br
-          a(href="/#production/interfaces/1/") Link to edit page
+          a(href="/#${path}/1/") Link to edit page
 `;
 
-    createFile('./app/pug/' + path, file, function () {
+    writeTemplate(basepath,file,template);
+}
+
+// Core functionality
+
+function writeTemplate(basepath,file,template) {
+    var fullpath = basepath + '/' + file;
+    createFile(basepath, file, function () {
         fs.writeFile(fullpath, template, function(err) {
             if(err) {
                 return console.log(err);
             }
-            console.log(fullpath, "saved!");
+            console.log(fullpath, "created successfully!");
         }); 
-    });
-}
-
-function createJsViewPageExtension(path,page) {
-    createFile('js/' + path, page + '.js', function () {
     });
 }
 
 function createFile(path, file,callback) {
     var fullpath = path + '/' + file;
+
+    var makefile = function () {
+
+        fs.open(fullpath,'ax', function (err, fd) {
+            if(fd == 13) {
+                if(callback)
+                    callback();
+            }
+            else {
+                console.log(fullpath, 'already exists, script did nothing.');
+            }
+        });
+    }
+
     fs.exists(path, (exists) => {
         if(!exists) {
-            console.log(path, 'not found, it will be created for you');
             mkdirp(path, function () {
-                console.log(path, 'created');
-                fs.readFile(fullpath, (err, data) => {
-                    if (err) {
-                        console.log(fullpath, ' not found, it will be created for you');
-                    }
-                    fs.open(fullpath,'a', function (err, fd) {
-                        if(fd == 13)
-                            console.log(fullpath, 'created');
-
-                        if(callback)
-                            callback();
-                    });
-                });
+                makefile();
             });
         } else {
-            console.log('now you can also create the file');
-
-            fs.readFile(fullpath, (err, data) => {
-                if (err) {
-                    console.log(fullpath, ' not found, it will be created for you');
-                }
-                fs.open(fullpath,'a', function (err, fd) {
-                    if(fd == 13)
-                        console.log(fullpath, 'created');
-
-                    if(callback)
-                        callback();
-                });
-            });
+            makefile();
         }
         
     });
@@ -146,4 +138,13 @@ function createFile(path, file,callback) {
 
 function getFileName(file,ext) {
     return file + '.' + ext;
+}
+
+function getTemplateId(name,page) {
+    var name = name.toUnderscore().replace(/\./g,'_');
+    return name + '_' + page;
+}
+
+function getPath(name) {
+ return name.toUnderscore().replace(/\./g,'/');
 }
